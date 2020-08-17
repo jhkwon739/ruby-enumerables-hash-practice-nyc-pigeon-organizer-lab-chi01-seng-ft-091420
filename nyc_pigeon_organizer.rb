1e3f79e4281a3cce6785f7bdd10d9a1a9ddccb8e
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-17 05:00:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,30 +5,31 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   pigeon_attributes, attributes_data = data.keys, data.values
   names = get_names(attributes_data).flatten.uniq
-  pigeon_list = condense_data(data, names, pigeon_attributes)
+  pigeon_list = organize_data(data, names, pigeon_attributes)
  # binding.pry
   
 end
 
+#return an array of all the names grabbed from pigeon data
 def get_names(attributes_array)
   attributes_array.map { |attribute| attribute.values}
 end
 
-def condense_data(data, names, pigeon_attributes)
-  new_hash = {}
+#returns new organized pigeon list
+def organize_data(data, names, pigeon_attributes)
+  pigeon_hash = {}
   names.map do |name|
-    new_hash[name] = {}
+    pigeon_hash[name] = {}
     pigeon_attributes.map do |attribute|
-      new_hash[name][attribute] = []
+      pigeon_hash[name][attribute] = []
       data[attribute].map do |specific_attribute, value|
         if value.include?(name)
-          #new_hash[name] = { attribute => [specific_attribute]}
-          new_hash[name][attribute] << specific_attribute.to_s
+          pigeon_hash[name][attribute] << specific_attribute.to_s
         end
       end
     end
   end
-  new_hash
+  pigeon_hash
 end
 
 
